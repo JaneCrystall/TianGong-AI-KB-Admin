783f4312c46934ad027c7aae059394f04aec8de9
nanli <linanenv@gmail.com>
2024-12-18 00:21:59 +0800

    Remove environment variable settings for Langchain tracing and API key in ESG module

--- src/esg/esg.py
+++ src/esg/esg.py
@@ -2,9 +2,6 @@ import os
 import streamlit as st
 from langgraph.pregel.remote import RemoteGraph
 
-os.environ["LANGCHAIN_TRACING_V2"] = "true"
-os.environ["LANGCHAIN_API_KEY"] = st.secrets["langsmith"]["api_key"]
-
 url = st.secrets["langgraph"]["url"]
 api_key = st.secrets["langgraph"]["api_key"]
 graph_name = "esg_search_agent"
